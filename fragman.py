1c3994d4045438468816605632d0a97c6c2f6492
Bar Goldenberg <bar.goldenberg@similarweb.com>
2024-02-21 15:40:19 +0200

    done

---
+++ fragman.py
@@ -0,0 +1,36 @@
+INCREMRENT_BALANCE_AMOUNT = 0.08
+
+def choose_item(costs, new_costs, count_player, balances):
+    amount_to_subtract = 0
+    for item in new_costs:
+        if new_costs[item] <= 0:
+            print("After adding", INCREMRENT_BALANCE_AMOUNT, "to each citizen", item, "is chosen.")
+            amount_to_subtract = costs[item] / count_player[item]
+            for i in range(len(votes)):
+                if item in votes[i]:
+                    balances[i] -= amount_to_subtract
+            break
+    
+def elect_next_budget_item(votes: list[set[str]], balances: list[float], costs: dict[str, float]):
+    for i in range(len(balances)):
+        balances[i] += INCREMRENT_BALANCE_AMOUNT
+    new_costs = costs.copy()
+    count_player = {}
+    for i in range(len(votes)):
+        for item in votes[i]:
+            new_costs[item] -= balances[i]
+            if item not in count_player:
+                count_player[item] = 1
+            else:
+                count_player[item] += 1
+    
+    choose_item(costs, new_costs, count_player, balances)
+    for i in range(len(balances)):
+        print("Citizen", i+1, "has", balances[i], "remaining balance.")
+
+
+votes = [{"Park in street X", "Fix road X"}, {"Park in street X"}]
+balances = [1, 1]
+costs = {"Park in street X": 2, "Fix road X": 3}
+
+elect_next_budget_item(votes, balances, costs)
