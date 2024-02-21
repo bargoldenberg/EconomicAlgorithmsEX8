4d748183f900018afde6a3f80c6e4fd52e47534b
Bar Goldenberg <bar.goldenberg@similarweb.com>
2024-02-21 22:17:04 +0200

    added test

--- fragman.py
+++ fragman.py
@@ -25,7 +25,6 @@ def elect_next_budget_item(votes: list[set[str]], balances: list[float], costs: 
                 count_player[item] = 1
             else:
                 count_player[item] += 1
-    
     choose_item(costs, new_costs, count_player, balances)
     for i in range(len(balances)):
         print("Citizen", i+1, "has", balances[i], "remaining balance.")
@@ -34,5 +33,12 @@ def elect_next_budget_item(votes: list[set[str]], balances: list[float], costs: 
 votes = [{"Park in street X", "Fix road X"}, {"Park in street X"}]
 balances = [1, 1]
 costs = {"Park in street X": 2, "Fix road X": 3}
+
+elect_next_budget_item(votes, balances, costs)
+print('------------------------')
+votes = [ {"Park", "Trees"}, {"Trees"}, {"Park", "Lights"}, {"Lights"}, {"Park"}]
+balances = [1.5, 2.4, 3.3, 4.2, 5.1]
+costs = {"Park": 1000, "Trees": 2000, "Lights": 3000}
+
 
 elect_next_budget_item(votes, balances, costs)
